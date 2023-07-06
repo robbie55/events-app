d9ae6d45d19791a5a1d201d0f1371dadb35edf21
Robbie McLaughlin <robbiemclaughlin@Robbies-MacBook-Pro.local>
2023-07-06 12:52:37 -0500

    added UI folder along with reuseable button and styles

---
+++ components/UI/button.js
@@ -0,0 +1,17 @@
+const { default: Link } = require("next/link");
+
+import classes from "./button.module.css";
+
+function Button(props) {
+  return (
+    <Link className={classes.btn} href={props.Link}>
+      {props.children}
+    </Link>
+  );
+}
+
+//Creating a resuable button to help UI and serve as a way to navigate pages
+//passing in props for the link href and props children to allow us to insert
+//dynamic text into the button
+
+export default Button;
