7b76c375c71af3866dc11a726655a89ec633bff5
Robbie McLaughlin <robbiemclaughlin@Robbies-MacBook-Pro.local>
2023-07-07 12:47:06 -0500

    added funcitonality to page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,32 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+
 function EventDetail() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>Chosen Event</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        alt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 }
 
