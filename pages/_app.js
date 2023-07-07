eb4c2d28700242768d6199bd4549c0b6b5829528
Robbie McLaughlin <robbiemclaughlin@Robbies-MacBook-Pro.local>
2023-07-07 12:59:26 -0500

    wrapped rendered pages with navbar component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import Layout from "@/components/layout/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+// root component where different componenets and pages are initially rendered
 
 export default MyApp;
