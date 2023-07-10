557eabd221ebea690f52a45a2915b24bb9444ebd
Robbie McLaughlin <robbiemclaughlin@Robbies-MacBook-Pro.local>
2023-07-10 11:24:04 -0500

    wired submit handler to page to allow rerouting through month and year

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
 import EventsList from "@/components/events/events-list";
+import { useRouter } from "next/router";
 import EventsSearch from "@/components/events/events-search";
 import { getAllEvents } from "@/dummy-data";
 
 function EventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventsList items={events} />
     </>
   );
